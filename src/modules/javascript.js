cddf3d678d31a1b1c80b95b9a0c774835f90a848
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-03-04 17:07:44 +0100

    Create method to convert temperature to fahreinheit

--- src/modules/javascript.js
+++ src/modules/javascript.js
@@ -3,4 +3,9 @@ function showWeatherIcon(image, title, element) {
     element.alt = title; 
 }
 
-export {showWeatherIcon}
+// Create method to convert temperature to Fahrenheit
+function convertToFahrenheit(tempt) {
+    return Math.round((tempt * (9/5) + 32) * 10) / 10 ; 
+}
+
+export {showWeatherIcon, convertToFahrenheit}
