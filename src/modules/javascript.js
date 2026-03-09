7c69eb424818ab873bb8914ef280807f3bd44b00
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-03-10 00:29:25 +0100

    Create reusable UI block for different hours

--- src/modules/javascript.js
+++ src/modules/javascript.js
@@ -4,5 +4,20 @@ function convertToFahrenheit(tempt) {
     return Math.round((tempt * (9/5) + 32) * 10) / 10 ; 
 }
 
+// Create a reusable UI block for creation of weather cards for each time interval
+const createHourCard = (data) => {
+  let iconImage = loadWeatherIcon(data.icon); 
+  return `
+	    <li class="hour-card">
+              <span>
+                    <div class="item interval">${data.time}</div>
+                    <div class="item weather-symbol">
+                        <img src="${iconImage} alt="${data.icon}">
+                    </div>
+                    <div class="item description">${data.description}</div>
+                </span>
+         </li>`;
+}
 
-export {showWeatherIcon, convertToFahrenheit}
+
+export {showWeatherIcon, createHourCard, convertToFahrenheit}
